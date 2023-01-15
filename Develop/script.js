dd1654bd02161b99da210a4f5017f6e490fc980a
mvill <119754846+migavg@users.noreply.github.com>
2023-01-14 18:45:13 -0700

    started project by adding a generate password function with a simple prompt to test application

---
+++ Develop/script.js
@@ -0,0 +1,28 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  
+  
+  prompt("How many characters would you like in your password?" );
+
+
+
+
+
+
+}
+
+// Write password to the #password input
+function writePassword() {
+ 
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
