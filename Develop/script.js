f4c6a8ecee8f4ca39d7132c076b4edff2e8ffe09
mvill <119754846+migavg@users.noreply.github.com>
2023-01-14 22:11:43 -0700

    Setup my prompts inside variables to have values set up for use in functions

--- Develop/script.js
+++ Develop/script.js
@@ -3,15 +3,24 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   
-  
-  prompt("How many characters would you like in your password?" );
+  var passwordLength = Number(prompt("How many characters would you like in your password?" ));
+  console.log(passwordLength);
 
+  var specialChar = confirm("Click OK to confirm including special characters.");
+  console.log(specialChar);
 
+  var numericChar = confirm("Click OK to confirm including numeric characters.");
+  console.log(numericChar);
 
+  var lowerChar = confirm("Click OK to confirm including lower case characters");
+  console.log(lowerChar);
 
-
+  var upperChar = confirm("Click OK to confirm including UPPER case characters");
+  console.log(upperChar);
 
 }
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -19,10 +28,10 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   
-
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
