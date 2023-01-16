d44773b2e946a0132d00da8c8fcdbc56e7ee00cb
mvill <119754846+migavg@users.noreply.github.com>
2023-01-16 13:59:06 -0700

    added some of the char variables needed for a password, stumped on how to run a for loop to generate that random text to the id selector

--- Develop/script.js
+++ Develop/script.js
@@ -1,64 +1,78 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-var  userInputs = {
-
-    passwordLengthMin: 8
+var passwordLength =10;
+// this is an object referencing the character set the user can choose from 
+var characterSet = {
+  upperChar: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lowerChar: "abcdefghijklmnopqrstuvwxyz",
+  specialChar: "!@#$%^&*()_+-=[]{};:'<>?",
+  numericChar: 0123456789
+}
+//  this is an object 
+var userInputs = {
+  passwordLengthMin: 8
 }
 
-var charTypes ={
-  lowerCase: "qwertyuiopasdfghjklzxcvbnm"
-}
+var customGeneratedPassword = "";
 
-function promptLength() {{
-    
-  var passwordLength = prompt("How many characters would you like in your password?" );
+// this function prompts a box asking user to input how many characters theyd like in their password 
+function promptLength() {
+
+  var passwordLength = prompt("How many characters would you like in your password?");
   console.log(passwordLength);
 
-  if (passwordLength < 8 || passwordLength >128 || passwordLength.isNAN) {
-    
-  promptLength();
-  alert("Please enter a number between 8-128");
-    }
-    else {
-   passwordLength = userInput.passwordLengthMin;
-      return userInputs.passwordLengthMin;
-    
-    }
-}
-}
-
-
-function generatePassword() {
-  
-  var text= "";
-  for (let i = 0; i < userLength.passwordLengthMin; i++){
-    text + = charTypes[(Math.floor(Math.random() * charTypes.length))];
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    alert("Please enter a number between 8-128.");
+    promptLength();
   }
-  return text;
-  
+  else {
+   return passwordLength;
+  }
+9
+}
 
-promptLength();  
 
-  var specialChar = confirm("Click OK to confirm including special characters.");
-  console.log(specialChar);
 
-  var numericChar = confirm("Click OK to confirm including numeric characters.");
-  console.log(numericChar);
 
-  var lowerChar = confirm("Click OK to confirm including lower case characters");
-  console.log(lowerChar);
+// this function generates the password length and outputs it into the generated box 
+function getPassword() {
+ 
+  for (var i = 0; i < passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * characterSet.upperChar.length);
+    customGeneratedPassword += characterSet.upperChar.substring(randomNumber,randomNumber+1 );
+}
 
-  var upperChar = confirm("Click OK to confirm including UPPER case characters");
-  console.log(upperChar);
+console.log(getPassword());
+}
+
+
+
+// this is the main function that is going to drive all the other functions 
+function generatePassword() {
+
+  promptLength();
+  generatePassword();
+
+  // var specialChar = confirm("Click OK to confirm including special characters.");
+  // console.log(specialChar);
+
+  // var numericChar = confirm("Click OK to confirm including numeric characters.");
+  // console.log(numericChar);
+
+  // var lowerChar = confirm("Click OK to confirm including lower case characters");
+  // console.log(lowerChar);
+
+  // var upperChar = confirm("Click OK to confirm including UPPER case characters");
+  // console.log(upperChar);
 
 }
+
 // Write password to the #password input
 function writePassword() {
- 
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
+
   passwordText.value = password;
 
 }
